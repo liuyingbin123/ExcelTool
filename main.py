b369f4a23e2e0a56b189f79e2ba474edc33d6931
liuyingbin@southzn.com <liuyingbin@southzn.com>
2021-07-27 18:13:06 +0800

    创建工程结构

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+    author:         liuyingbin
+    project_name:   ExcelTool
+    data:           2021/7/27
+    company:        SouthSmart
+    CopyRight:      Doc LIU
+"""
+"""
+...主逻辑处理入口
+"""
+def main():
+    pass
