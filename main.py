48aa2c14288da68179f2703f21118435ddc7d189
liuyingbin@southzn.com <liuyingbin@southzn.com>
2021-07-27 19:02:00 +0800

    增加测试数据

--- main.py
+++ main.py
@@ -5,8 +5,26 @@
     company:        SouthSmart
     CopyRight:      Doc LIU
 """
+import argparse
+import xlrd
 """
 ...主逻辑处理入口
 """
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='输出参数列表')
+    parser.add_argument('-dataTablePath', default='./data/data.xlsx',type=str, help='基础数据表输入路径... '
+                                                                        'for example:-dataTablePath test.xlsx')
+    parser.add_argument('-outputPath', default='./test.xlsx', type=str, help='数据输出路径')
+    args = parser.parse_args()
+    # 创建结果表
+    csvfile = file('csv_test.csv', 'wb')
+    csvfile.write(codecs.BOM_UTF8)
+    writer = csv.writer(csvfile)
+    list = ['姓名', '年龄', '电话']
+    writer.writerow(list)
+    csvfile.close()
+
+if __name__ == '__main__':
+    main()
